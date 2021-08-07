8f9e1ac0642c7c422e60831380e3d0979b8fc435
Maliha Sultana <malihasultana998@gmail.com>
2021-08-07 23:22:50 +0600

    Revert "soda"
    This reverts commit 5d6b3c36c991b155a142906650ac1c4e7a257b3e.

--- hungry.py
+++ hungry.py
@@ -4,6 +4,4 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
 else:
-    thirsty=input("are you thirsty?")
-    if thirsty=="yes":
-        print("drink water")
+    print("do homework")
